cbbcbf8acefe284c6d1c25946014b79b80b07670
Arnaldvm <arnald7v@hotmail.com>
2015-12-12 18:06:29 +0100

    First commit
    We can start working (We can, this doesn't mean we are gonna work)

---
+++ Physics3D_class6 - solution/ModuleSceneIntro.cpp
@@ -0,0 +1,63 @@
+#include "Globals.h"
+#include "Application.h"
+#include "ModuleSceneIntro.h"
+#include "Primitive.h"
+#include "PhysBody3D.h"
+
+ModuleSceneIntro::ModuleSceneIntro(Application* app, bool start_enabled) : Module(app, start_enabled)
+{
+}
+
+ModuleSceneIntro::~ModuleSceneIntro()
+{}
+
+// Load assets
+bool ModuleSceneIntro::Start()
+{
+	LOG("Loading Intro assets");
+	bool ret = true;
+
+	App->camera->Move(vec3(1.0f, 1.0f, 0.0f));
+	App->camera->LookAt(vec3(0, 0, 0));
+
+	//s.size = vec3(100, 0.5f, 100);
+	//s.SetPos(0, 0.5f, 20);
+	c.size = vec3(5, 1, 3);
+	c.SetPos(0, 3, 20);
+
+	
+
+	sensor = App->physics->AddBody(c, 0.0f);
+	sensor->SetAsSensor(false);
+	sensor->collision_listeners.add(this);
+
+	return ret;
+}
+
+// Load assets
+bool ModuleSceneIntro::CleanUp()
+{
+	LOG("Unloading Intro scene");
+
+	return true;
+}
+
+// Update
+update_status ModuleSceneIntro::Update(float dt)
+{
+	Plane p(0, 1, 0, 0);
+	p.axis = true;
+	p.Render();
+
+	sensor->GetTransform(&s.transform);
+	s.Render();
+	c.Render();
+
+	return UPDATE_CONTINUE;
+}
+
+void ModuleSceneIntro::OnCollision(PhysBody3D* body1, PhysBody3D* body2)
+{
+	LOG("Hit!");
+}
+
